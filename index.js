3362bcaa105e9aadb70ea893b0b3d8d1ee65b23d
Nataliya R <n.rudenko@gmail.com>
2019-09-17 01:18:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+class Driver {
+  constructor (name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  }
+}
